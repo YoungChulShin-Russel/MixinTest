3c147b9c14564393706a04dfc57244bb5f4276f3
unknown <youngchul.shin@meshkorea.net>
2019-11-21 05:45:23 +0900

    01.기본정책을 구현하는 추상 클래스 추가

---
+++ Test.scala
@@ -0,0 +1,7 @@
+abstract class BasicRatePolicy {
+
+    def calculateFee(phone: Phone): Money = 
+        phone.calls.map(calculateCallFee(_)).reduce(_ + _)
+
+    protected def calculateCallFee(call: Call): Money;
+}
